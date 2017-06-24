f01b07815e09ad6c8f19012446096d7afea11a2e
renatasarmet <renatassmendes@hotmail.com>
2017-06-24 13:45:20 -0300

    Funcao 2 testes prontos

--- funcaoNewton/funcao2.R
+++ funcaoNewton/funcao2.R
@@ -1,5 +1,3 @@
-#TEM QUE OLHAR DE NOVO
-
 source("automatizacaoZeroFuncao.R");
 
 f <- function(x) { x - x*log(x) };
@@ -7,10 +5,10 @@ df <- function(x) { -log(x) - 1 };
 ddf <-function(x) { -1/x - 1 };
 
 #Não é possível determinar chute de intervalo bom
-a <- 2;
+a <- 2.1;
 b <- 3;
 
-#Teste bom de chute pois é bem perto do resultado
+#Teste bom de chute para secante porém ruim para Newton
 x0 <- 1.6;
 x1 <- 2;
 
--- funcaoNewton/funcao2outroTeste.R
+++ funcaoNewton/funcao2outroTeste.R
@@ -1,10 +1,20 @@
-funcao2 <- function(x){ x - x*log(x) };
-df2 <- function(x) { -log(x) - 1 };
-ddf2 <-function(x) { -1/x - 1 };
-print("xNewton: chute inicial pra newton\nxBissecao[0]: inferior\nxBissecao[1]: superior\nxSecante[0]: inferior\nxSecante[1]: superior");
+source("automatizacaoZeroFuncao.R");
 
-xNewton <- 2.5;
-xBisseccao[0] <- 2;
-xBisseccao[1] <- 3;
-xSecante[0] <- 2;
-xSecante[1] <- 3;
+f <- function(x) { x - x*log(x) };
+df <- function(x) { -log(x) - 1 };
+ddf <-function(x) { -1/x - 1 };
+
+#Não é possível determinar chute de intervalo bom. Apesar do intervalo agora ser bem maior, o numero de iteraçoes mudou pouco
+a <- 0.2;
+b <- 100;
+
+#Teste ruim de chute para Newton e pior para Secante, porém a Secante anda rápido mesmo assim. Tambem percebemos que Newton teve mais ou menos o mesmo tanto de iteração tanto com teste longe quanto com teste ruim; o problema dele foi próximo ao resultado final
+x0 <- 100;
+x1 <- 200;
+
+#Tambem podemos perceber pelo gráfico que o chute tem que ser depois de 1, pois se nao causa indefinido (será que convem fazer um terceiro teste sobre isso?)
+
+#Tolerancia proxima ao zero de maquina
+tol <- 10^-16;
+
+automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
