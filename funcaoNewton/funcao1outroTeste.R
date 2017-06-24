8189933d05a87e3819c0d01cbd304131eddb419f
renatasarmet <renatassmendes@hotmail.com>
2017-06-24 01:19:11 -0300

    Funcao1outroTeste editada com sucesso

--- funcaoNewton/funcao1outroTeste.R
+++ funcaoNewton/funcao1outroTeste.R
@@ -4,11 +4,15 @@ f <- function(x) { x^5 - (10*x^3)/9 + 5/21 };
 df <- function(x) { 5*x^4 - (10*x^2)/3};
 ddf <- function(x) { 20*x - 20*x/3 };
 
-#Teste ruim
+#Teste ruim de intervalo, pois ele chega quase no resultado, entao fica muito tempo por ir sempre na metade
 a <-0.8;
 b <- 0.9;
-x0 <- 0.8;
-x1 <- 1;
+
+#Teste ruim de chute pois é bem longe do resultado
+x0 <- 50;
+x1 <- 45;
+
+#Tolerancia proxima ao zero de maquina
 tol <- 10^-16;
 
 automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
