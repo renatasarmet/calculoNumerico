ab847709c63f5be01cabbf95bfa095c3a5b80ae5
renatasarmet <renatassmendes@hotmail.com>
2017-06-26 14:59:13 -0300

    Update funcao5.R

--- funcaoNewton/funcao5.R
+++ funcaoNewton/funcao5.R
@@ -4,11 +4,11 @@ f <- function(x) { x^11 - 0.001 };
 df <- function(x) { 11*x^10 };
 ddf <- function(x) { 110*x^9 };
 
-#Teste ruim de intervalo, pois ele chega quase no resultado, entao fica muito tempo por ir sempre na metade
+#VER
 a <- -1;
 b <- 1;
 
-# Teste bom de chute pois é perto do resultado
+#VER
 x0 <- 0;
 x1 <- 0.5;
 
