11284017acb2aa0ad9b577c4c16cfe9e31909d8f
renatasarmet <renatassmendes@hotmail.com>
2017-06-26 15:16:59 -0300

    Mucanda grau aumentado funcao1outroTeste

--- funcaoNewton/funcao1outroTeste.R
+++ funcaoNewton/funcao1outroTeste.R
@@ -1,18 +1,20 @@
 source("automatizacaoZeroFuncao.R");
 
-f <- function(x) { x^5 - (10*x^3)/9 + 5/21 };
-df <- function(x) { 5*x^4 - (10*x^2)/3};
-ddf <- function(x) { 20*x - 20*x/3 };
+f <- function(x) { x^50 - (10*x^30)/9 + 5/21 };
+df <- function(x) { 50*x^49 - (100*x^29)/3};
+ddf <- function(x) { 2450*x^48 - (2900*x^28)/3 };
 
-#Intervalo nao converge pois f(a) * f(b) > 0
-a <- 0.7;
-b <- 0.9;
+#REESCREVER TUDO COM A FUNCAO NOVA
 
-#Teste ruim de chute pois é bem longe do resultado
-x0 <- 50;
-x1 <- 45;
+##Intervalo nao converge pois f(a) * f(b) > 0
+#a <- 0.7;
+#b <- 0.9;
 
-#Tolerancia proxima ao zero de maquina
-tol <- 10^-16;
+##Teste ruim de chute pois é bem longe do resultado
+#x0 <- 50;
+#x1 <- 45;
 
-automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
+##Tolerancia proxima ao zero de maquina
+#tol <- 10^-16;
+
+#automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
