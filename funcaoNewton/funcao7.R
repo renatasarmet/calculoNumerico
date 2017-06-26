4b8d1517be5140d8e8e083f5d74e293e62db463e
renatasarmet <renatassmendes@hotmail.com>
2017-06-26 15:07:29 -0300

    Create funcao7.R

---
+++ funcaoNewton/funcao7.R
@@ -0,0 +1,18 @@
+source("automatizacaoZeroFuncao.R");
+
+f <- function(x) { 1/tg(x) };
+df <- function(x) { -csc(x)^2  };
+ddf <- function(x) { 2*cot(x) * csc(x)^2 };
+
+#VER
+a <- 1;
+b <- 3;
+
+#VER
+x0 <- 1;
+x1 <- 2;
+
+# Tolerancia proxima ao zero de maquina
+tol <- 10^-16;
+
+automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
