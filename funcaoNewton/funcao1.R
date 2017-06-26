1e3fa42468fe4ba9d56d4a416e84349deb0ff737
renatasarmet <renatassmendes@hotmail.com>
2017-06-26 15:15:46 -0300

    Mudanca aumentando grau funcao1

--- funcaoNewton/funcao1.R
+++ funcaoNewton/funcao1.R
@@ -1,18 +1,20 @@
 source("automatizacaoZeroFuncao.R");
 
-f <- function(x) { x^5 - (10*x^3)/9 + 5/21 };
-df <- function(x) { 5*x^4 - (10*x^2)/3};
-ddf <- function(x) { 20*x - 20*x/3 };
+f <- function(x) { x^50 - (10*x^30)/9 + 5/21 };
+df <- function(x) { 50*x^49 - (100*x^29)/3};
+ddf <- function(x) { 2450*x^48 - (2900*x^28)/3 };
 
-#Teste ruim de intervalo, pois ele chega quase no resultado, entao fica muito tempo por ir sempre na metade
-a <- 0.8;
-b <- 0.9;
+#REESCREVER TUDO COM A FUNCAO NOVA
+
+##Teste ruim de intervalo, pois ele chega quase no resultado, entao fica muito tempo por ir sempre na metade
+#a <- 0.8;
+#b <- 0.9;
 
 # Teste bom de chute pois é perto do resultado
-x0 <- 0.83;
-x1 <- 1;
+#x0 <- 0.83;
+#x1 <- 1;
 
-# Tolerancia proxima ao zero de maquina
-tol <- 10^-16;
+## Tolerancia proxima ao zero de maquina
+#tol <- 10^-16;
 
-automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
+#automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
