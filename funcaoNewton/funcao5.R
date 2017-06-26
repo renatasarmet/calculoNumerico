f5434b139b2161c13b3d506ef247b963c5fb6528
renatasarmet <renatassmendes@hotmail.com>
2017-06-26 09:11:41 -0300

    Delete funcao5.R

--- funcaoNewton/funcao5.R
+++
@@ -1,9 +0,0 @@
-source("automatizacaoZeroFuncao.R");
-
-funcao5 <- function(x) { x^4 - 6*x^3 + 10*x^2 - 6*x + 9 };
-df5 <- function(x) { 4*x^3 - 18*x^2 + 20*x - 6 };
-ddf5 <- function(x) { 12*x^2 - 36*x + 20 };
-
-xNewton <- 1.5;
-
-automatiza(f,df,ddf,a,b,x0,x1,tol,tol,tol,tol,tol,tol,tol,tol,tol);
