16e13b003177ad7500ae4954ded37db6d9597533
João Gabriel Melo Barbirato <joaobarbirato@gmail.com>
2017-06-26 10:45:30 -0300

    Update funcao3.R

--- funcaoNewton/funcao3.R
+++ funcaoNewton/funcao3.R
@@ -5,12 +5,18 @@ df <-function (x) { exp(x)/2 + 2*sin(x) };
 ddf<-function (x) { exp(x)/2 + 2*cos(x) };
 
 # Teste ruim, provavelmente por causa do método
+# todos os testes não se diferenciaram muito entre si
+# nesse teste temos um intervalo pequeno.
 a <- 0.904;
 b <- 0.9048;
 
-# Teste ruim de chute pois é bem longe do resultado
-x0 <- 0;
-x1 <- -1;
+# Testei perto da parte predominantemente exponencial 
+# (pior das hipóteses) (derivada maior) e, fora os casos de
+# troca de sentido da concavidade, quando não é possível 
+# aplicar o método,
+# os métodos de newton e secante aparentam não mudar muito.
+x0 <- .8;
+x1 <- 10;
 
 # Tolerancia proxima ao zero de maquina
 tol <- 10^-16;
